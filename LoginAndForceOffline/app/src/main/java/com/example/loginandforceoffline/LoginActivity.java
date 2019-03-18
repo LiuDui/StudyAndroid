9c1c15cfcf4682e7904a31fcbb2a362121122a41
LiuRui <liurui@novastar.tech>
2019-03-18 16:19:06 +0800

    在强制下线练习中，增加密码栏回显星号，增加记住密码功能

--- LoginAndForceOffline/app/src/main/java/com/example/loginandforceoffline/LoginActivity.java
+++ LoginAndForceOffline/app/src/main/java/com/example/loginandforceoffline/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.loginandforceoffline;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,16 +15,36 @@ public class LoginActivity extends BaseActivity {
     private EditText accountEdit = null;
     private EditText passwordEdit = null;
 
+    private CheckBox checkBoxRemeberPassword = null;
     private Button btnLogin = null;
+
+    private SharedPreferences sharedPreferences = null;
+    private SharedPreferences.Editor editor = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         accountEdit =findViewById(R.id.account_edittext);
         passwordEdit = findViewById(R.id.password_edittext);
         btnLogin = findViewById(R.id.btn_login);
+        checkBoxRemeberPassword = findViewById(R.id.checkbox_remeber_password);
+
+        boolean isRemeber = sharedPreferences.getBoolean("remeber_password", false);
+
+        if (isRemeber){
+            String account = sharedPreferences.getString("account", "");
+            String password = sharedPreferences.getString("password", "");
+
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+
+            checkBoxRemeberPassword.setChecked(true);
+        }
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +52,17 @@ public class LoginActivity extends BaseActivity {
                 String account = accountEdit.getText().toString();
                 String password = passwordEdit.getText().toString();
 
-                boolean isTrue = AccountUtil.isVild(account, password);
-                if (isTrue){
+                if (AccountUtil.isVild(account, password)){
+                    editor = sharedPreferences.edit();
+                    if (checkBoxRemeberPassword.isChecked()){
+                        editor.putBoolean("remeber_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    }else{
+                        editor.clear();
+                    }
+                    editor.apply();
+
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
@@ -39,5 +71,6 @@ public class LoginActivity extends BaseActivity {
                 }
             }
         });
+
     }
 }
